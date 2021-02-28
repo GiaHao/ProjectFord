38f969161c954383b5575af2fd419310c60be95b
GiaHao <pgiahao.95@gmail.com>
2021-02-28 18:07:22 +0700

    them lan 1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById("play-video").addEventListener("click", play)
+
+function play() {
+    var video = document.getElementById("message__video");
+    video.play();
+    this.style.display = "none";
+    video.controls = true;
+};
