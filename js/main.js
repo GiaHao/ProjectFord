4ef0e872451dbdd6bc813efad63e8a30ed837fca
GiaHao <pgiahao.95@gmail.com>
2021-03-01 13:22:17 +0700

    them section va chinh sua

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-document.getElementById("play-video").addEventListener("click", play)
+// Nút play trong Message
+document.getElementById("play-video").addEventListener("click", play);
 
 function play() {
-    var video = document.getElementById("message__video");
-    video.play();
-    this.style.display = "none";
-    video.controls = true;
-};
+  var video = document.getElementById("message__video");
+  video.play();
+  this.style.display = "none";
+  video.controls = true;
+}
+
+// Hình nền khi nhấn dropdown menu
+
+
+function clickDropdownMenu() {
+    var container = document.getElementById("container-bg");
+
+    container.classList.add("container-background");
+    
+}
+
+window.onclick = function (e) {
+    var container = document.getElementById("container-bg");
+    if (!e.target.matches('.nav-item')) {
+          if (container.classList.contains('container-background')) {
+            container.classList.remove('container-background');
+          }
+      }
+}
