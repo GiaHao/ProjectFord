1ac49d6c0d280ab01b911b03c92be3e298d8e1a3
GiaHao <pgiahao.95@gmail.com>
2021-03-01 23:15:03 +0700

    chinh sua cuoi

--- js/main.js
+++ js/main.js
@@ -7,22 +7,3 @@ function play() {
   this.style.display = "none";
   video.controls = true;
 }
-
-// Hình nền khi nhấn dropdown menu
-
-
-function clickDropdownMenu() {
-    var container = document.getElementById("container-bg");
-
-    container.classList.add("container-background");
-    
-}
-
-window.onclick = function (e) {
-    var container = document.getElementById("container-bg");
-    if (!e.target.matches('.nav-item')) {
-          if (container.classList.contains('container-background')) {
-            container.classList.remove('container-background');
-          }
-      }
-}
